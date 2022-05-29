38d1653b5476a6807ad47659debd75a7bd69b2a1
Rijan984 <rijan18shrestha@gmail.com>
2022-05-29 23:15:07 +0545

    transparent background with onClick effect

--- src/components/blog/Popup.js
+++ src/components/blog/Popup.js
@@ -3,41 +3,44 @@ import './popup.css';
 
 function Popup({ info, setShow }) {
   return (
-    <div className="mainBlog-popup">
-      <div className="blogPopup">
-        <img
-          src="https://bdnmb.ca/wp-content/uploads/2020/06/pexels-photo-106399.jpeg"
-          alt="Property"
-        />
-        <div className="info">
-          <div className="top">
-            <p className="btn btn-primary"> Purpose</p>
-            <p className="btn btn-primary"> Purpose</p>
-            <p className="btn btn-primary"> Purpose</p>
-          </div>
-          <div className="description">
-            <p>{info.title}</p>
-            <span>{info.address}</span>
-            <span>State</span>
-            <span>District</span>
-            <p> Rating and comment</p>
-            <span>Price</span>
-            <span className="btn btn-primary">Negotiable</span>
-            <div>emojis</div>
-            <p>description</p>
-            <p>Tota Area: </p>
-            <p>Tota Area: </p>
-            <p>Tota Area: </p>
-            <p>Tota Area: </p>
-            <p>Tota Area: </p>
-          </div>
-          <div className="exit">
-            <button
-              className="btn btn-primary close"
-              onClick={() => setShow(false)}
-            >
-              Close
-            </button>
+    <div>
+      <div className="backDrop" onClick={() => setShow(false)}></div>
+      <div className="mainBlog-popup">
+        <div className="blogPopup">
+          <img
+            src="https://bdnmb.ca/wp-content/uploads/2020/06/pexels-photo-106399.jpeg"
+            alt="Property"
+          />
+          <div className="info">
+            <div className="top">
+              <p className="btn btn-primary"> Purpose</p>
+              <p className="btn btn-primary"> Purpose</p>
+              <p className="btn btn-primary"> Purpose</p>
+            </div>
+            <div className="description">
+              <p>{info.title}</p>
+              <span>{info.address}</span>
+              <span>State</span>
+              <span>District</span>
+              <p> Rating and comment</p>
+              <span>Price</span>
+              <span className="btn btn-primary">Negotiable</span>
+              <div>emojis</div>
+              <p>description</p>
+              <p>Tota Area: </p>
+              <p>Tota Area: </p>
+              <p>Tota Area: </p>
+              <p>Tota Area: </p>
+              <p>Tota Area: </p>
+            </div>
+            <div className="exit">
+              <button
+                className="btn btn-primary close"
+                onClick={() => setShow(false)}
+              >
+                Close
+              </button>
+            </div>
           </div>
         </div>
       </div>
